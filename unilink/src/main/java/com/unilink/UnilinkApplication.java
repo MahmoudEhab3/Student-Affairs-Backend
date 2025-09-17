51a078f0cfd954729f764dd905d39d2c497a876e
Ahmed hegazy <121024792+hegzo891@users.noreply.github.com>
2025-09-17 13:16:36 +0300

    change password from the mail

--- unilink/src/main/java/com/unilink/UnilinkApplication.java
+++ unilink/src/main/java/com/unilink/UnilinkApplication.java
@@ -2,8 +2,11 @@ package com.unilink;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
+
 public class UnilinkApplication {
 
 	public static void main(String[] args) {
---
+++ unilink/src/main/java/com/unilink/dto/ResetPasswordRequest.java
@@ -0,0 +1,9 @@
+package com.unilink.dto;
+
+import lombok.Data;
+
+@Data
+public class ResetPasswordRequest {
+    private String token;
+    private String newPassword;
+}
